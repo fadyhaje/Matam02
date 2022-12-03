6532a635b99e31837eaa6ce8e7ba289e54f74fb9
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2022-12-03 23:16:30 +0200

    Create Card.c

---
+++ Card.c
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Card.h"
+#include "utilities.h"
+
+
+Card ::Card(CardType type, const CardStats& stats)
+{
+ this->m_effect=type;
+ this->m_stats=stats;
+}
+void Card ::applyEncounter(Player& player) const
+{
+if ( m_effect== CardType::Battle)
+{
+if( player.getAttackStrength()>=m_stats.force)
+{printBattleResult(true);
+player.levelUp();
+player.addCoins(m_stats.loot);
+}
+else {
+    printBattleResult(false);
+    player.damage(m_stats.hpLossOnDefeat);
+}
+}
+else
+    if ( m_effect==CardType::Heal|| m_effect==CardType::Buff)
+    {
+        if(player.pay(m_stats.cost))
+        {
+            if(m_effect==CardType::Heal)
+            {  player.heal(m_stats.heal);
+            }
+            else{
+                player.buff(m_stats.buff);
+            }
+        }
+
+    }
+    else if (m_effect==CardType::Treasure)
+    {
+        player.addCoins(m_stats.loot);
+
+    }
+}
+
+void Card ::printInfo() const
+{
+
+    if(m_effect==CardType::Battle)
+    {
+        printBattleCardInfo(m_stats);
+    }
+    else
+        if(m_effect==CardType::Heal)
+        {
+            printHealCardInfo(m_stats);
+        }
+        else
+            if(m_effect==CardType::Buff)
+            {
+                printBuffCardInfo(m_stats);
+            }
+            else
+                if(m_effect==CardType::Treasure)
+                {
+                    printTreasureCardInfo(m_stats);
+                }
+
+}
