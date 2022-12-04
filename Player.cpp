31c23013b3a6ee6cc046b73ac10b6d1b36a00ab6
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2022-12-04 12:08:17 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,7 +2,9 @@
 #include "Player.h"
 
 Player ::Player(char* name,int maxHP,int force){
-    this->name=name;
+    int len = strlen(name);
+    this->name = new char[len+1];
+    strcpy(this->name,name);
     this->level=1;
     this->coins=0;
     if(force>0){
@@ -18,7 +20,7 @@ void Player::printInfo(){
     printfPlayerInfo(name,level,force,HP,coins);
 }
 
-void Player::levelUP(){
+void Player::levelUP(){///////
     if(level<10){
         level++;
     }
@@ -27,10 +29,40 @@ int Player::getLevel(){
     return level;
 }
 
+Player::~Player(){
+    delete [] name;
+}
 void Player::buff(int addedForce){
     if(addedlForce>=0){
     force+=addedlForce;
     }
+}
+
+Player::Player (const Player& player){
+    int len = strlen(player.name);
+    name = new char[len+1];
+    strcpy(name,player.name);
+    m_level = player.level ;
+    m_coins = player.coins ;
+    m_force = player.force ;
+    m_maxHP = player.maxHP ;
+    m_HP = player.HP ;
+}
+
+Player& Player::operator=(const Player& player) {
+	if (this == &player) {
+		return *this;
+	}
+	delete [] name;
+    int len = strlen(player.name);
+	name = new char[len+1];
+    strcpy(name,player.name);
+	level = player.level ;
+    coins = player.coins ;
+    force = player.force ;
+    maxHP = player.maxHP ;
+    HP = player.HP ;
+	return *this;
 }
 
 void Player::heal(int aaddedHP){
@@ -56,7 +88,7 @@ void Player::damage(int lowHP){
 }
 
 bool Player::isKnouckedOut(){
-    if(HP<=0){
+    if(HP==0){
         return true;
     }
     return false;
@@ -68,8 +100,8 @@ void Player::addCoins(int money){
     }
 }
 
-bool PLayer::pay(int money){//there is a differnce
-    if(money>=0){//>= \\>?
+bool PLayer::pay(int money){//there is a differnce//////
+    if(money>=0 ){//>= \\>?
         if(coins>=money){
             coins-=money;
             return true;
@@ -79,5 +111,5 @@ bool PLayer::pay(int money){//there is a differnce
 }
 
 int Player::getAttackStrength(){
-    return level+force;
+    return (level+force);
 }
