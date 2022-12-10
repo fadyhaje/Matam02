1c74ca78b927a9a6767bc8f88e8986a096ce216a
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2022-12-10 14:14:22 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,10 +1,3 @@
-//
-//  Player.h
-//  MTMCHKIN
-//
-//  Created by fady haje on 05/05/2022.
-//
-
 #ifndef Player_h
 #define Player_h
 #include <string.h>
@@ -24,21 +17,21 @@ public:
     /**
      * @brief Construct a new Player object
      *
-     * @param name - name of the player
-     * @param maxHP - Player's max hp (default = 100)
+     * @param name - the name of the player
+     * @param maxHP - the max hp of the player (default = 100)
      * @param force - the force of the player(default = 5)
      */
     Player(const char* name,int maxHP=100,int force=5);
    
     /**
-     * @brief Copy Player object
+     * @brief Copy constructor of the Player 
      *
-     * @param player - the player we want to copy
+     * @param player - the player theat will be copied
      */
     Player(const Player& player);
 
     /**
-     * @brief Destroy the Player object
+     * @brief Destroy the Player 
      *
      */
     ~Player();
@@ -46,20 +39,20 @@ public:
     /**
      * @brief assignment operator
      *
-     * @param other - the other player we want to make player equal to
+     * @param other - the other is a player that will be equedled to another player  
      *
      * @return Player& - player that we want to make it equal to other
      */
     Player& operator=(const Player& other);
 
     /**
-     * @brief  print the info of the player
+     * @brief  print the information about the player
      *
      */
     void printInfo();
 
     /**
-     * @brief level up the level of the player
+     * @brief make the level up of the player
      *
      */
     void levelUp();
@@ -67,28 +60,28 @@ public:
     /**
      * @brief Get the Level of player
      *
-     * @return int returns player's level
+     * @return int returns the level of the player
      */
     int getLevel() const;
 
     /**
      * @brief buff the players force by additional force
      *
-     * @param additionalForce - the amount of force we want to add to player
+     * @param additionalForce - the amount of force that will be added to the player
      */
     void buff(int additionalForce);
 
     /**
      * @brief heal the player by additional hp
      *
-     * @param additionalHP - the amount of hp we want to add to player
+     * @param additionalHP - the amount of hp that will be added to player
      */
     void heal(int additionalHP);
 
     /**
      * @brief damage player by damageHP
      *
-     * @param damageHP - the amount of damage we want to deduce from player's HP
+     * @param damageHP - the amount of damage that will be deduced from the player
      */
     void damage(int damageHP);
 
@@ -96,33 +89,33 @@ public:
      * @brief returns if player is knocked out
      *
      * @return true - player is knocked out
-     * @return false - player is not knocked out
+     * @return false - player isn't knocked out
      */
     bool isKnockedOut() const;
 
     /**
-     * @brief adds coins to player by additional coins
+     * @brief adds coins to the player by additional coins
      *
-     * @param additionalCoins - the amount of gold we want to add to player
+     * @param additionalCoins - the amount of gold that willl be added to the player
      */
     void addCoins(int additionalCoins);
 
     /**
-     * @brief makes player pay
+     * @brief the player pay amount of money equals to damageConis
      *
-     * @param damageCoins - the amount of gold we want to deduce from the player
+     * @param damageCoins - the amount of gold that will be deduced from the player
      * @return true - if the player can afford the amount of gold
-     * @return false - if the player can't afford the amount of gold
+     * @return false - if the player can not afford the amount of gold
      */
     bool pay(int damageCoins);
 
     /**
-     * @brief Get the Attack Strength of player
+     * @brief Get the Attack Strength of the player
      *
-     * @return int returns Attack strength of player
+     * @return int returns the Attack strength of the player
      */
     int getAttackStrength();
-         Player()  {}
+         //Player()  {}
 
 };
 
